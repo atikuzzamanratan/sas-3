8937cf0f78a03a8481a2c161865537c380d1ef36
Atikuzzaman Ratan <azamanru@gmail.com>
2026-04-21 09:59:45 +0600

    Login page support number updated.

--- Components/footer-credential.php
+++ Components/footer-credential.php
@@ -1,8 +1,8 @@
 
 <footer style="text-align: center; background-color: #f1f1f1">
     <div style="text-align: center;">
-        <p>Support : <a href="tel:+8801973390330">+88 01973390330</a> || Sales : <a href="tel:+8801953561249">+88
-                01953561249</a> <br/>
+        <p>Support : <a href="tel:+8801720540859">+88 01720 540859</a> <!--|| Sales : <a href="tel:+8801953561249">+88
+                01953561249</a>--> <br/>
             <a href="../TermsConditions.php" target="_blank">Terms & Conditions</a> ||
             <a href="../PrivacyPolicy.php" target="_blank">Privacy Policy</a> <br/>
             <a href="https://solversbd.com/" target="_blank">SOLVERS</a>
